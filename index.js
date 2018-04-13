b5c8fb03d48c95e772d317d3c6741f6d36a750f1
Mike C <mikecronin92@gmail.com>
2018-04-13 03:41:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return newObj;
 }
 
-funtion destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
